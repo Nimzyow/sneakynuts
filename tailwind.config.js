2ffaf3bfc6d9ffa763b0cb39dc9893e720f7059f
Nima Soufiani <n_soufiani@hotmail.com>
2025-07-20 21:42:37 +0100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,ts,tsx,vue,svelte}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#3D5A80",
+        secondary: "#98C1D9",
+        accent: "#E0FBFC",
+        highlight: "#EE6C4D",
+        dark: "#293241",
+      },
+    },
+  },
+};
