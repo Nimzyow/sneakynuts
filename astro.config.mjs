953bb233e21a3df1383b2d494adf5c4f36901506
Nima Soufiani <n_soufiani@hotmail.com>
2025-09-05 13:10:37 +0100

    progress

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,24 +4,4 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   integrations: [],
-  content: {
-    collections: {
-      blog: {
-        type: 'content',
-        schema: {
-          type: 'object',
-          properties: {
-            title: { type: 'string' },
-            description: { type: 'string' },
-            author: { type: 'string' },
-            pubDate: { type: 'string', format: 'date' },
-            tags: { type: 'array', items: { type: 'string' } },
-            emoji: { type: 'string' },
-            featured: { type: 'boolean' }
-          },
-          required: ['title', 'description', 'author', 'pubDate']
-        }
-      }
-    }
-  }
 });
