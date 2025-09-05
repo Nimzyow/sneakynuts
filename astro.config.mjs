111f419838471d5dca2196cc0c1b3957361ac0b5
Nima Soufiani <n_soufiani@hotmail.com>
2025-09-05 13:20:16 +0100

    progress

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,23 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import remarkGfm from 'remark-gfm';
+import rehypeSlug from 'rehype-slug';
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [],
+  markdown: {
+    shikiConfig: {
+      theme: 'github-dark',
+      wrap: true
+    },
+    gfm: true,
+    smartypants: true,
+    remarkPlugins: [remarkGfm],
+    rehypePlugins: [
+      rehypeSlug,
+      [rehypeAutolinkHeadings, { behavior: 'wrap' }]
+    ],
+  }
 });
